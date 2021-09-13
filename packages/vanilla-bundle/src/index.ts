83ce4615252dc65cf870bd097fa3103e1fa96be5
Marcos Callegario <mcallegario@gmail.com>
2021-09-12 23:58:18 -0300

    include  slickgrid-excelExport in vanilla-bundle

--- packages/vanilla-bundle/src/index.ts
+++ packages/vanilla-bundle/src/index.ts
@@ -5,6 +5,7 @@ import { SlickCompositeEditorComponent } from '@slickgrid-universal/composite-ed
 import { SlickEmptyWarningComponent } from '@slickgrid-universal/empty-warning-component';
 import { SlickPaginationComponent } from '@slickgrid-universal/pagination-component';
 import { SlickVanillaGridBundle } from './components/slick-vanilla-grid-bundle';
+import { ExcelExportService } from '@slickgrid-universal/excel-export';
 
 const Slicker = {
   GridBundle: SlickVanillaGridBundle,
@@ -22,11 +23,13 @@ const Slicker = {
 // expose the bundle on the global "window" object as Slicker
 if (typeof window !== 'undefined') {
   (window as any).Slicker = Slicker;
+  (window as any).ExcelExportService = ExcelExportService;
 }
 
 export { BindingService };
 export { Aggregators, Editors, Enums, EventPubSubService, Filters, Formatters, GroupTotalFormatters, SortComparers, Utilities };
 export { SlickCompositeEditorComponent, SlickEmptyWarningComponent, SlickPaginationComponent, SlickVanillaGridBundle }; // export the custom components & interfaces
 export { Slicker };
+export { ExcelExportService };
 export * from './interfaces/index';
 export * from './services/index';
