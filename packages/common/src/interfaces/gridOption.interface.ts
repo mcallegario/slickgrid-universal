c2e342e9a4047d66698c2b3db2d928da00022300
Marcos Callegario <mcallegario@gmail.com>
2021-11-26 23:37:54 -0300

    fix(Sanitize):Do we want to enable Sanitize Html

--- packages/common/src/interfaces/gridOption.interface.ts
+++ packages/common/src/interfaces/gridOption.interface.ts
@@ -352,6 +352,9 @@ export interface GridOption {
   /** Do we want to enable row selection? */
   enableRowSelection?: boolean;
 
+  /** Do we want to enable Sanitize Html */
+  enableSanitizeHtml?: boolean;
+
   /** Do we want to enable sorting? */
   enableSorting?: boolean;
 
